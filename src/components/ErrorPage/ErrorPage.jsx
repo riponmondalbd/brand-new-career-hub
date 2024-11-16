107b2a4c38bd1e9cf6a8a31cf90c800c1459b9a8
riponmondalbd <riponmondal149@gmail.com>
2024-11-17 00:14:19 +0600

    complete error page ui design

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,14 +1,28 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
   const error = useRouteError();
   return (
-    <div>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    // <div>
+    //   <h1>Oops!</h1>
+    //   <p>Sorry, an unexpected error has occurred.</p>
+    //   <p>
+    //     <i>{error.statusText || error.message}</i>
+    //   </p>
+    // </div>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Oops!</h1>
+          <p className="py-2">Sorry, an unexpected error has occurred.</p>
+          <p className="py-6">
+            <i>{error.statusText || error.message}</i>
+          </p>
+          <Link to={-1}>
+            <button className="btn btn-primary">Go Back</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
