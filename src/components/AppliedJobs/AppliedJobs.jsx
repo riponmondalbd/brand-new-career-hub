830e3f8afd2077d225acc7ed764c501931f137ce
riponmondalbd <riponmondal149@gmail.com>
2024-11-18 03:33:38 +0600

    job filter done

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -4,16 +4,43 @@ import { getJobsFromLocalStorage } from "../utility/localStorage";
 import DetailsAppliedJobs from "./DetailsAppliedJobs/DetailsAppliedJobs";
 
 const AppliedJobs = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const jobs = useLoaderData();
   const [appliedJobs, setAppliedJobs] = useState([]);
+  const [displayJobs, setDisplayJobs] = useState([]);
 
   useEffect(() => {
     const storedJobs = getJobsFromLocalStorage();
     if (jobs.length > 0) {
       const jobApplied = jobs.filter((job) => storedJobs.includes(job.id));
       setAppliedJobs(jobApplied);
+      setDisplayJobs(jobApplied);
     }
   }, [jobs]);
+
+  const toggleDropDown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeDropDown = () => {
+    setIsOpen(false);
+  };
+
+  const handleJobsFilter = (filter) => {
+    if (filter === "all") {
+      setDisplayJobs(appliedJobs);
+    } else if (filter === "remote") {
+      const remoteJobs = appliedJobs.filter(
+        (job) => job.remote_or_onsite === "Remote"
+      );
+      setDisplayJobs(remoteJobs);
+    } else if (filter === "onsite") {
+      const onsiteJobs = appliedJobs.filter(
+        (job) => job.remote_or_onsite === "Onsite"
+      );
+      setDisplayJobs(onsiteJobs);
+    }
+  };
 
   return (
     <div>
@@ -23,8 +50,36 @@ const AppliedJobs = () => {
         </h1>
       </div>
       <div className="max-w-7xl mx-auto py-[130px]">
+        <div className="flex justify-end">
+          <div className="dropdown dropdown-bottom dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn m-1"
+              onClick={toggleDropDown}
+            >
+              Filter By
+            </div>
+            {isOpen && (
+              <ul
+                tabIndex={0}
+                className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+              >
+                <li onClick={() => handleJobsFilter("all")}>
+                  <a onClick={closeDropDown}>All</a>
+                </li>
+                <li onClick={() => handleJobsFilter("remote")}>
+                  <a onClick={closeDropDown}>Remote</a>
+                </li>
+                <li onClick={() => handleJobsFilter("onsite")}>
+                  <a onClick={closeDropDown}>Onsite</a>
+                </li>
+              </ul>
+            )}
+          </div>
+        </div>
         <div>
-          {appliedJobs.map((jobs) => (
+          {displayJobs.map((jobs) => (
             <DetailsAppliedJobs key={jobs.id} jobs={jobs}></DetailsAppliedJobs>
           ))}
         </div>
